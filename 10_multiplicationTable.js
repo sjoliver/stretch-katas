f427c8e91018049bbb6577646e5425c93f87d1eb
Sophie <sophiejoliver19+1@gmail.com>
2022-01-03 23:34:02 +0000

    function for building a multiplication table

---
+++ 10_multiplicationTable.js
@@ -0,0 +1,14 @@
+const multiplicationTable = function(maxValue) {
+  
+  let number = maxValue;
+  
+  while (number > 0) {
+
+
+  }
+
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
